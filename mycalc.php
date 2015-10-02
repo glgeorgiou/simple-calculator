6745d9d4ac68c73754f6bfb7218f607f94f771d3
GGEORGIOU <ggeorgiou.gr@gmail.com>
2015-10-02 17:36:04 +0300

    fix: Better form field validation plus new design mockup

--- mycalc.php
+++ mycalc.php
@@ -9,16 +9,53 @@
 <div class="container">
 	<div class="content">
     
-    <?php 
-//Variable to print the result
-$result = 0;
-
+<!-- The calculator form-->
+<form name="form1" method="post" action="mycalc.php">
+  <table border="0">
+    <tr>
+      <td>Number 1</td>
+      <td>Op.</td>
+      <td>Num 2</td>
+    </tr>
+    <tr>
+      <td><input class="fieldborder" type="text" name="num1"  size="10" maxlength="10" value="0"></td>
+      <td id="op"><label for="select"></label>
+        <select class="fieldborder" name="operand" id="select">
+          <option>+</option>
+          <option>-</option>
+          <option>*</option>
+          <option>/</option>
+      </select></td>
+      <td><input class="fieldborder" type="text" name="num2"  size="10" maxlength="10" value="0"></td>
+    </tr>
+    <tr>
+      <td><input type="submit" name="compute"  value="Compute"></td>
+      <td class="mytd" colspan="2">
+<?php  //Controls wether variables are empty or not
 //Check text boxes
 
-if ( (isset($_POST['num1']) && !empty($_POST['num1'])) || (isset($_POST['num2']) && !empty($_POST['num2']))) {
-	$result = calc($_POST['num1'], $_POST['num2'], $_POST['op']);
-	} 
+if ( (isset($_POST['num1']) && !empty($_POST['num1'])) && (isset($_POST['num2']) && !empty($_POST['num2']))) 
+{
+	if (is_numeric($_POST['num1']) && is_numeric($_POST['num2'])) 
+	{
+		$result = calc($_POST['num1'], $_POST['num2'], $_POST['operand']);
+		echo '<b>Result</b><br /> '.$_POST['num1']. ' '. $_POST['operand']. ' ' .$_POST['num2']. " = $result"; 
+	} else 
+	{
+		echo "At least one number is not numeric";
+	}
 	
+}  else 
+{
+	echo "Please set both numbers";
+}
+?></td>
+    </tr>
+  </table>
+</form>
+    
+    <?php 
+
 	//Calculation function
 
 function calc($num1, $num2, $op)
@@ -52,38 +89,7 @@ function calc($num1, $num2, $op)
 
 
 
-?>
-
-    
-<!-- The calculator form-->
-<form name="form1" method="post" action="mycalc.php">
-  <table border="0">
-    <tr>
-      <td>Number 1</td>
-      <td>Op.</td>
-      <td>Num 2</td>
-    </tr>
-    <tr>
-      <td><input class="fieldborder" type="text" name="num1"  size="10" maxlength="10" value="0"></td>
-      <td id="op"><label for="select"></label>
-        <select class="fieldborder" name="op" id="select">
-          <option>+</option>
-          <option>-</option>
-          <option>*</option>
-          <option>/</option>
-      </select></td>
-      <td><input class="fieldborder" type="text" name="num2"  size="10" maxlength="10" value="0"></td>
-    </tr>
-    <tr>
-      <td><input type="submit" name="compute"  value="Compute"></td>
-      <td class="mytd" colspan="2"><?php  //Controls wether variables are empty or not
-	  if (isset($_POST['num1']) and isset($_POST['op']) and isset($_POST['num2']))
-echo '<b>Result</b><br /> '.$_POST['num1']. ' '. $_POST['op']. ' ' .$_POST['num2']. " = $result"; 	  
-	  ?></td>
-    </tr>
-  </table>
-</form>
-    
+?>    
 	</div>
   <div class=".footer">Use "." instead of "," for decimals</div>
 </div>
